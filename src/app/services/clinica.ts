e3ce191b7db3dfa90876ff9df1c0cbd2468eaf6c
Tu Nombre <tu_correo_de_github@loquesea.com>
2025-11-18 17:38:06 -0500

    servicio clinica

--- src/app/services/clinica.ts
+++ src/app/services/clinica.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Clinica } from '../models/clinica.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Clinica {
-  
+export class ClinicaService {
+
+  private apiUrl = 'http://localhost:8080/api/clinicas';
+
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Clinica[]> {
+    return this.http.get<Clinica[]>(this.apiUrl);
+  }
+
+  crear(clinica: Clinica): Observable<Clinica> {
+    return this.http.post<Clinica>(this.apiUrl, clinica);
+  }
+
+  obtenerPorId(id: number): Observable<Clinica> {
+    return this.http.get<Clinica>(`${this.apiUrl}/${id}`);
+  }
+
+  actualizar(id: number, clinica: Clinica): Observable<Clinica> {
+    return this.http.put<Clinica>(`${this.apiUrl}/${id}`, clinica);
+  }
+
+  eliminar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
